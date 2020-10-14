e0395188ecb7d304a160d8bda4e8196e59980b9f
Marcelo8173 <marceloandrebio@gmail.com>
2020-10-13 22:03:07 -0400

    landing css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './styles/global.css';
+import './pages/landing.css'
+import LogoImg from './assets/Logo.png';
+import {Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={LogoImg} alt="Logo"/>
+
+        <main>
+          <h1 id="title">Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Manaus</strong>
+          <span>Amazonas</span>
+        </div>
+
+        <a href="/" className="enter-to-page">
+
+        </a> 
+      </div>
     </div>
   );
 }
