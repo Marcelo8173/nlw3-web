7fc90a4fbdbfbce8b5d148d5a4190b63934cad6a
Marcelo8173 <marceloandrebio@gmail.com>
2020-10-14 15:18:25 -0400

    styles of list map

--- src/pages/ListMap/ListMap.tsx
+++ src/pages/ListMap/ListMap.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-
+import Marke from '../../assets/Local.png'
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import './ListMap.css';
 
 const ListMap = () => {
     return(
-        <h1>olá </h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={Marke} alt=""/>
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Manaus</strong>
+                    <span>Amazonas</span>
+                </footer>
+            </aside>
+
+            <div></div>
+            <Link to="/" className="create">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
     )
 }
 
